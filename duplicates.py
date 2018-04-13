69acfdca513da80f60a55bcb62cdc42e82a5c3f1
Stanislav <37021640+oeananas@users.noreply.github.com>
2018-04-13 11:24:18 +0300

    Update duplicates.py
    corrected the names of variables, applied collections.defaultdict(list) in the function get_files_name_size_path(directory)

--- duplicates.py
+++ duplicates.py
@@ -1,27 +1,24 @@
 import os
 import sys
+import collections
 
 
-def get_files_size_path(directory):
-    file_info = {}
-    for dirs_path, dirs, files in os.walk(directory):
-        for file in files:
-            filepath = os.path.join(dirs_path, file)
-            size = os.path.getsize(filepath)
-            file_size = file + '+' + str(size)
-            if file_size not in file_info:
-                file_info[file_size] = [dirs_path]
-            else:
-                file_info[file_size].append(dirs_path)
-    return file_info
+def get_files_name_size_path(directory):
+    files_info_dic = collections.defaultdict(list)
+    for dirs_path, dirs, names in os.walk(directory):
+        for file_name in names:
+            file_path = os.path.join(dirs_path, file_name)
+            file_size = os.path.getsize(file_path)
+            file_name_and_size = (file_name, file_size)
+            files_info_dic[file_name_and_size].append(dirs_path)
+    return files_info_dic
 
 
 def get_duplicates(file_info):
     duplicates = {}
     for info, path in file_info.items():
         if len(path) > 1:
-            file_size = info.split('+')
-            duplicates[file_size[0]] = path
+            duplicates[info[0]] = path
     return duplicates
 
 
@@ -31,9 +28,10 @@ if __name__ == '__main__':
     directory = sys.argv[1]
     if not os.path.isdir(directory):
         exit('wrong path to the directory / directory not exist')
-    file_size_path = get_files_size_path(directory)
+    file_size_path = get_files_name_size_path(directory)
     duplicates = get_duplicates(file_size_path)
     if not duplicates:
         exit('no duplicates in such directory')
+    print('duplicate files found:')
     for file, path in duplicates.items():
         print(file, path)
